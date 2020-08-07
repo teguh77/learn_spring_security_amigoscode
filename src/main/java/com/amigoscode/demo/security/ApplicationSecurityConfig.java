41f048fe00888fd5688d46d1d99bb2aa7db2fb40
Tama THK <teguhpratama337@gmail.com>
2020-08-07 20:31:08 +0700

    first commit auth_basic

---
+++ src/main/java/com/amigoscode/demo/security/ApplicationSecurityConfig.java
@@ -0,0 +1,93 @@
+package com.amigoscode.demo.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRepository;
+
+import static com.amigoscode.demo.security.ApplicationUserPermission.COURSE_WRITE;
+import static com.amigoscode.demo.security.ApplicationUserRole.*;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+//                .csrf().csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                /* ini hanya digunakan apabila ingin mengakses server dari client (ANGULAR)
+                   kalau tidak maka sebaiknya tidak usah disetting, soalnya csrf sudah enable by default
+                */
+                .csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
+                .anyRequest()
+                .authenticated()
+                .and()
+                .httpBasic();
+        /* *cara baca: -kita akan mengauthorize request -> .authorizeRequests()
+        *              -untuk any request  -> .anyRequest()
+        *              -authenticate request tersebut -> .authenticated()
+        *              -dan -> .and()
+        *              -gunakan authentikasi dengan mekanisme basic auth (http basic) -> .httpBasic();
+        *              -dengan menggunakan httpbasic ini kita akan login tidak menggunakan form tapi
+        *                   dengan menggunakan pop up, dan tidak bisa di logout, karena username dan password akan
+        *                   di kirim terus menerus ke server
+        * // ini merupakan setting pada security basic
+        * - Untuk mendefinisikan antmatcher maka kita harus memperhatikan urutan dari yang paling spesifik ke yang paling umum
+        */
+
+    }
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("pass"))
+//                .roles(STUDENT.name()) // ROLE_STUDENT
+                .authorities(STUDENT.getGrantedAuthorities())
+                .build();
+
+        UserDetails lindaUser = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("pass"))
+//                .roles(ADMIN.name()) // ROLE_ADMIN
+                .authorities(ADMIN.getGrantedAuthorities())
+                .build();
+
+        UserDetails tomUser = User.builder()
+                .username("tom")
+                .password(passwordEncoder.encode("pass"))
+//                .roles(ADMINTRAINEE.name()) // ROLE_ADMINTRAINEE
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+                .build();
+
+        return new InMemoryUserDetailsManager(
+                annaSmithUser,
+                lindaUser,
+                tomUser
+        );
+    }
+}
